330295ea78a7550c072c9a5affbd4c01a8b4697b
Alexander Pavlitskiy <alex.pavlitskiy@mail.ru>
2024-10-25 22:57:05 +0300

    Initial Commit

---
+++ Hackathon Autumn 2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackathon Autumn 2024
+//
+//  Created by Александр Павлицкий on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
